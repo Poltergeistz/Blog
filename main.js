35ee1ddcca2021206b9dcbdccbff939f8a984161
david <david.journo.31@gmail.com>
2018-06-03 03:15:03 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function checkForm(form){
+    if(form.inputfield.value == ""){
+        alert("Error : input is empty!");
+        form.inputfield.focus();
+        return false;
+    }
+
+    if(regExp);
+}
